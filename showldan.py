7b7e2bc3b69d1f7e61a27533f6605cd02849bbd0
S1rN3tZ <40497633+mathis2001@users.noreply.github.com>
2023-03-02 09:50:04 +0100

    Adding of options
    options added => no preffix tag for target, search by organization name.

--- showldan.py
+++ showldan.py
@@ -8,10 +8,12 @@ api = shodan.Shodan(SHODAPI)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--target", help="Target domain", type=str)
+parser.add_argument("-O", "--organization", help="Target organization name", action="store_true")
 parser.add_argument("-s", "--exposed-services", help="Search for exposed services", action="store_true")
 parser.add_argument("-p", "--default-pass", help="Search for default passwords", action="store_true")
 parser.add_argument("-i", "--info-disclosure", help="Search for information disclosure", action="store_true")
 parser.add_argument("-d", "--domain-takeover", help="Search for domain takeover", action="store_true")
+parser.add_argument("-n", "--no-prefixtag", help="Search without tag before target", action="store_true")
 parser.add_argument("-l", "--limit", help="Limit of results", type=str)
 args= parser.parse_args()
 
@@ -37,9 +39,9 @@ def banner():
 	⠀⠀⠀⠀⠘⣷⡀⠈⣿⡀⠈⠀⠀⠀⠛⠛⠁⠀⠀⠁⠀⣾⠃⢀⣼⠇⠀⠀|⠀ -i, --info-disclosure		     |
 	⠀⠀⠀⠀⠀⠘⢷⣄⠘⣷⡀⢀⡀⠀⡀⠀⠀⠀⡀⠀⣼⠏⣠⡾⠋⠀⠀⠀|⠀⠀-d, --domain-takeover	             |
 	⠀⠀⠀⠀⠀⠀⠀⠙⢷⣼⣷⡌⠻⠟⠁⠀⠻⠿⢃⣼⣯⡾⠛⠁⠀⠀⠀⠀|  -l, --limit                           | ⠀⠀
-	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⣿⠿⣶⣄⣀⣀⣠⣴⡟⢿⡍⠀⠀⠀⠀⠀  |________________________________________|
-	⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠏⢀⣿⠉⢹⡏⠉⢹⡇⠈⢿⡄⠀⠀⠀⠀
-	⠀⠀⠀⠀⠀⠀⠀⠀⠘⠋⠀⠈⠃⠀⠘⠃⠀⠈⠃⠀⠈⠛⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⣿⠿⣶⣄⣀⣀⣠⣴⡟⢿⡍⠀⠀⠀⠀⠀  |  -O, --organization                    |
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠏⢀⣿⠉⢹⡏⠉⢹⡇⠈⢿⡄⠀⠀⠀⠀  |  -n, --no-prefixtag                    |
+	⠀⠀⠀⠀⠀⠀⠀⠀⠘⠋⠀⠈⠃⠀⠘⠃⠀⠈⠃⠀⠈⠛⠀⠀⠀⠀⠀ |________________________________________|
 		  by S1rN3tZ
 
 	''')
@@ -64,45 +66,76 @@ def ShodanSearch(target, search):
 
 def main():
 	if args.target:
-		prefix = ['ssl.cert.subject.CN:.','hostname:*.']
-		targetBySSL = prefix[0]+args.target
-		targetByHost = prefix[1]+args.target
+		if args.no_prefixtag:
+			prefix = []
+			target = args.target
+		elif args.organization:
+			prefix = ["org:"]
+			targetorg = prefix[0]+args.target
+		else:
+			prefix = ['ssl.cert.subject.CN:.','hostname:*.']
+			targetBySSL = prefix[0]+args.target
+			targetByHost = prefix[1]+args.target
 		if args.exposed_services:
 			print(bcolors.INFO+"[*] "+bcolors.RESET+'Searching exposed services for '+bcolors.INFO+args.target+bcolors.RESET)
 			with open("exposed_services.txt", "r") as services:
 				for search in services:
-					print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[0]+bcolors.RESET)
-					ShodanSearch(targetBySSL, search)
-					print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[1]+bcolors.RESET)
-					ShodanSearch(targetByHost, search)
+					if args.no_prefixtag:
+						ShodanSearch(target, search)
+					elif args.organization:
+						print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[0]+bcolors.RESET)
+						ShodanSearch(targetorg, search)
+					else:
+						print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[0]+bcolors.RESET)
+						ShodanSearch(targetBySSL, search)
+						print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[1]+bcolors.RESET)
+						ShodanSearch(targetByHost, search)
 
 		if args.default_pass:
 			print(bcolors.INFO+"\n[*] "+bcolors.RESET+'Searching default passwords for '+bcolors.INFO+args.target+bcolors.RESET)
 			with open("default_pass.txt", "r") as defpass:
 				for search in defpass:
-					print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[0]+bcolors.RESET)
-					ShodanSearch(targetBySSL, search)
-					print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[1]+bcolors.RESET)
-					ShodanSearch(targetByHost, search)
+					if args.no_prefixtag:
+						ShodanSearch(target, search)
+					elif args.organization:
+						print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[0]+bcolors.RESET)
+						ShodanSearch(targetorg, search)
+					else:
+						print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[0]+bcolors.RESET)
+						ShodanSearch(targetBySSL, search)
+						print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[1]+bcolors.RESET)
+						ShodanSearch(targetByHost, search)
 
 
 		if args.info_disclosure:
 			print(bcolors.INFO+"\n[*] "+bcolors.RESET+'Searching information disclosure for '+bcolors.INFO+args.target+bcolors.RESET)
 			with open("info_disclosure.txt", "r") as disclosure:
 				for search in disclosure:
-					print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[0]+bcolors.RESET)
-					ShodanSearch(targetBySSL, search)
-					print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[1]+bcolors.RESET)
-					ShodanSearch(targetByHost, search)
+					if args.no_prefixtag:
+						ShodanSearch(target, search)
+					elif args.organization:
+						print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[0]+bcolors.RESET)
+						ShodanSearch(targetorg, search)
+					else:
+						print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[0]+bcolors.RESET)
+						ShodanSearch(targetBySSL, search)
+						print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[1]+bcolors.RESET)
+						ShodanSearch(targetByHost, search)
 
 		if args.domain_takeover:
 			print(bcolors.INFO+"\n[*] "+bcolors.RESET+'Searching domain takeover for '+bcolors.INFO+args.target+bcolors.RESET)
 			with open("domain_takeover.txt", "r") as takeover:
 				for search in takeover:
-					print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[0]+bcolors.RESET)
-					ShodanSearch(targetBySSL, search)
-					print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[1]+bcolors.RESET)
-					ShodanSearch(targetByHost, search)
+					if args.no_prefixtag:
+						ShodanSearch(target, search)
+					elif args.organization:
+						print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[0]+bcolors.RESET)
+						ShodanSearch(targetorg, search)
+					else:
+						print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[0]+bcolors.RESET)
+						ShodanSearch(targetBySSL, search)
+						print(bcolors.INFO+"[*] "+bcolors.RESET+'Prefix used: '+bcolors.INFO+prefix[1]+bcolors.RESET)
+						ShodanSearch(targetByHost, search)
 
 
 try:
